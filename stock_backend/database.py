ea9ccc2467cfdd1c34a7b5172564c3de721528d1
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-02-14 23:46:26 -0300

    import settings class instance

--- stock_backend/database.py
+++ stock_backend/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from stock_backend.settings import Settings
+from stock_backend.settings import settings
 
-engine = create_engine(Settings().DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 
 
 def get_session():
